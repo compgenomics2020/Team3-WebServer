6b90f8f83bd23afd0c09b6c9eed88aebe4f79866
Allison N. Rozanski <arozanski3@gatech.edu>
2020-04-17 09:24:56 -0500

    updated to move email to backend

--- webserver/backend/routes.py
+++ webserver/backend/routes.py
@@ -1,11 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint Response
 
 #from api.models import Process_File
 
 mod=Blueprint('backend',__name__)
 
-@mod.route('/getStuff',methods=['GET', 'POST'])
-def getStuff(path,filename):
-	if request.method == 'POST':
-		return '{"result" : "You are in the API!!!}'
+@mod.route('/send_message_backend')
+def send_message_back(email,filename):
+	msg=Message(
+		subject='Scolia output',
+		sender='scoliagatech@gmail.com',
+		reciepients=
+			[email])
+	with mod.open_resource("../downloads/test.txt") as fp:
+		msg.attach("test.txt",test/txt",fp.read())
+	mail.send(msg)
+	confirm_msg = 'Your message has been sent!"
+	return (True)
+@mod.route("/download_display")
+def download_display():
+	return'''
+	<html><body>
+	Hello. <a href="/backend/get_Output">Click me.</a>
+	</body></html>
+	'''
+@mod.route('/get_Output'):
+def get_Output():
+	with mod.open_resource("../downloads/test.txt") as fp:
+                test=fp.read()
+	return Response(
+		test,
+		mimetype="text/plain",
+		headers={"Content-disposition":
+			"attachment; filename=my_test.txt"})
 
--- webserver/frontend/routes.py
+++ webserver/frontend/routes.py
@@ -3,7 +3,7 @@ from flask_mail import Mail, Message,current_app
 
 mod=Blueprint('frontend',__name__,template_folder='templates',static_folder='static',static_url_path='/frontend/static')
 from webserver import mail
-
+from webserver.backend import routes as backend_mod
 app = Flask(__name__)
 
 
@@ -15,20 +15,14 @@ def homepage():
 def genomeassembly():
     return render_template("GenomeAssembly.html")
 
-@mod.route("/send_message_assem",methods=['POST'])
+@mod.route("/send_message",methods=['POST'])
 def send_message():
-	email=request.form.get('assem_email')
-	msg = Message(
-		subject='Scolia output',
-		sender='scoliagatech@gmail.com',
-		recipients=
-			[email])
-		#html=render_template("index.html"))
-	with mod.open_resource("../downloads/test.txt") as fp:
-			msg.attach("test.txt", "test/txt", fp.read())
-	mail.send(msg)
-	confirm_msg = 'Your message has been sent!'
-	return render_template("index.html", confirm_msg=confirm_msg)
+	email_usr=request.form.get('assem_email')
+	directory="../downloads"
+	did_send=backend_mod.send_message_back(email_usr,directory)
+	if did_send:
+		confirm_msg='Your message has been sent!'
+		return render_template("submit.html",confirm_msg=confirm_msg)
 
 @mod.route("/geneprediction")
 def geneprediction():
