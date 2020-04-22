c5052c351b115259fb5f50779c15bccc8d9a5887
Allison N. Rozanski <arozanski3@gatech.edu>
2020-04-21 21:52:39 -0500

    added genome assembly functionality in

--- webserver/backend/routes.py
+++ webserver/backend/routes.py
@@ -70,18 +70,20 @@ def generate_job_id():
 ##########################################LOOK FROM HERE#############################################
 
 @mod.route('/backend_assembly')
-def backend_assembly(new_filename, user_email,pipeline_num,tools,file1_location,download_folder):
-    flag=0
-    #MAKE OUTPUT PATH SPECIFIC FOR YOUR TOOL THIS IS JUST A TEST OUTPUT PATH
-    #subprocess.run("mkdir "+BASE_OUTPUT_PATH+"Genome_Assembly/"+new_filename, shell = True)
-    output_path=BASE_OUTPUT_PATH+"Genome_Assembly/"+new_filename+".tar.gz"
-    #THIS IS JUST AN EXAMPLE FUNCTION
-    pool.apply_async(models.f,(10,file1_location,flag,output_path))
+def backend_assembly(new_filename, user_email, pipeline_num, tools, file1_location, download_folder):
+    print("running assembly back_end.....")
+    print("tools are " + str (tools))
+    flag = 0
+	# MAKE OUTPUT PATH SPECIFIC FOR YOUR TOOL THIS IS JUST A TEST OUTPUT PATH
+    subprocess.run("mkdir " + BASE_OUTPUT_PATH + "Genome_Assembly/" + new_filename, shell=True)
+    output_path = BASE_OUTPUT_PATH + "Genome_Assembly/" + new_filename + ".tar.gz"
+    print("this is the output_path"+output_path)
+	# THIS IS JUST AN EXAMPLE FUNCTION
+    pool.apply_async(genomeassembly.f, (file1_location, flag, output_path, tools))
     if flag == 0:
-    	c1 = db_util.scolia_data(job_id = new_filename, email = user_email ,job_submitted = 0, email_sent = 0, pipeline_number = pipeline_num)
+    	c1 = db_util.scolia_data(job_id=new_filename, email=user_email, job_submitted=0, email_sent=0,pipeline_number=pipeline_num)
     	db_util.insert(c1)
-    return(True)
-
+    return (True)
 @mod.route('/backend_prediction')
 def backend_prediction(new_filename, user_email,pipeline_num,tools,file1_location,file2_location,download_folder):
     flag=0
